f69935357240b4512f22c20d31e48967b5c63feb
Tom-Nagy <tomnagy@hotmail.fr>
2022-02-20 07:48:51 +0000

    Add style to admin links

--- products/views.py
+++ products/views.py
@@ -101,9 +101,8 @@ def add_product(request):
             messages.success(request, 'Product added successfully')
             return redirect(reverse('product_detail', args=[product.id]))
         else:
-            messages.error(request,
-                           ('Failed to add product. '
-                            'Please ensure the form is valid.'))
+            messages.error(request, 'Failed to add product.'
+                                    'Please ensure the form is valid.')
     else:
         form = ProductForm()
 
