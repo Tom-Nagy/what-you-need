90bef374ea113d62ead444f9b77e6fe434c6507a
Tom-Nagy <tomnagy@hotmail.fr>
2022-02-20 09:03:30 +0000

    Add environment variables to set DEBUG and SECRET_KEY for live and development

--- what_you_need/settings.py
+++ what_you_need/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9*$(_^mcmj203b9(b(lly(35j(0%w_%u2p&_uxkx5a%6fgkrkl'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['what-you-need.herokuapp.com', 'localhost']
 
